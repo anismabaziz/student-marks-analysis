b793b88647bfec989a4d60811e18484b82c169d8
deathstarset <anisbaziz.sub@gmail.com>
2025-03-21 10:24:36 +0100

    Remove Analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import Dashboard from "./app/dashboard/page";
-import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     <>
       <Dashboard />
-      <Analytics />
     </>
   );
 }
