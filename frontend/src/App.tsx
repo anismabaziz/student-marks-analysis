a51a7b682a47947ab1274d02d55adc89a51a67ad
deathstarset <anisbaziz.sub@gmail.com>
2025-03-21 10:21:56 +0100

    Add Analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Dashboard from "./app/dashboard/page";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     <>
       <Dashboard />
+      <Analytics />
     </>
   );
 }
